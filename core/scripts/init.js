7a5cd42289f77bc67703a32e7e6f06bd8615e69f
Alex Petrosh <petrosh@users.noreply.github.com>
2016-04-22 17:14:03 +0200

    Serve DEBUG mode and use short SHA 🔡

--- core/scripts/init.js
+++ core/scripts/init.js
@@ -47,7 +47,8 @@ var gfl = {
       }
     };
     apiCall.err = function(){
-      console.log(this.responseText);
+      // Bad credentials, revoked token
+      gfl.login();
     };
     apiCall.call();
     return "called repo";
@@ -81,24 +82,30 @@ var gfl = {
     apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/git/refs/heads/master";
     apiCall.cb = function(){
       repo.ref = JSON.parse( this.responseText );
-      monitor( 'version', repo.ref.object.sha );
+      monitor( 'version', repo.ref.object.sha.substring(0,7) );
       gfl.checkFile('leagues.owner.js');
     };
     apiCall.call();
   },
   checkFile: function(file){
-    // load leagues.owner.js
-    apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/contents/core/scripts/" + file;
-    apiCall.data = '{"ref":"master"}';
-    apiCall.cb = function(){
-      var script = JSON.parse( this.responseText );
-      console.log(script);
-      // var script = document.createElement("script");
-      // script.type = 'text/javascript';
-      // script.innerHTML = "console.log('eddai');";
-      // document.body.appendChild(script);
-    };
-    apiCall.call();
+    if(url.array[0]=='127'){
+      var script = document.createElement("script");
+      script.src = file;
+      script.type = 'text/javascript';
+      document.body.appendChild(script);
+    }else{
+      // load leagues.owner.js
+      apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/contents/core/scripts/" + file;
+      apiCall.data = '{"ref":' + repo.ref.object.sha + '}';
+      apiCall.cb = function(){
+        var script = JSON.parse( this.responseText );
+        var added = document.createElement("script");
+        added.type = 'text/javascript';
+        added.innerHTML = atob(script.content);
+        document.body.appendChild(added);
+      };
+      apiCall.call();
+    }
   },
   guestConnected: function(){
     console.log("guest connected");
@@ -108,7 +115,7 @@ var gfl = {
     apiCall.cb = function(){
       parent.ref = JSON.parse( this.responseText );
       monitor( 'forked from', '<a href="http://' + repo.content.parent.owner.login + '.github.io/' + repo.content.parent.name + '">' + repo.content.parent.owner.login + '/' + repo.content.parent.name + '</a>' );
-      monitor( 'parent <em>master</em> version', parent.ref.object.sha );
+      monitor( 'parent <em>master</em> version', parent.ref.object.sha.substring(0,7) );
       gfl.checkSHA();
     };
     apiCall.err = function(){
@@ -126,7 +133,7 @@ var gfl = {
         monitor( 'version', repo.ref.object.sha );
         // checkSettings();
       }else{
-        monitor( '<em>master</em> version', 'need update from ' + repo.ref.object.sha );
+        monitor( '<em>master</em> version', 'need update from ' + repo.ref.object.sha.substring(0,7) );
         gfl.update();
         // update();
       }
