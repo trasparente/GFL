9b64d49d14c4cca07c155a56e7e95cc9ca83b970
Alex Petrosh <petrosh@users.noreply.github.com>
2016-04-25 19:26:20 +0200

    Simplify flow 🐴

--- core/scripts/init.js
+++ core/scripts/init.js
@@ -1,171 +1,93 @@
+// init.js
+
 var user = {},
   apiCall = {},
   parent = {};
 
-//
-// GFL
-//
 var gfl = {
   start: function(){
+    if(localStorage.getItem( 'gfl.user.token')) user.token = atob( localStorage.getItem( 'gfl.user.token' ) );
+    if(!url.page) url.script='home'; else url.script=url.page;
     apiCall.accept = 'application/vnd.github.v3.full+json';
     apiCall.method = 'GET';
-    if ( localStorage.getItem( 'gfl.user.token' ) ) return this.getThisRepo(); else return this.login();
+    apiCall.data = '';
+    apiCall.url = repo.API + "/git/refs/heads/master";
+    apiCall.cb = function(){
+      repo.ref = JSON.parse( this.responseText );
+      repo.sha = repo.ref.object.sha;
+      gfl.getThisRepo();
+    };
+    apiCall.err = function(){
+      monitor( 'SHA', 'no SHA' );
+      gfl.getThisRepo();
+    };
+    apiCall.call();
   },
   getThisRepo: function(){
-    user.token = atob( localStorage.getItem( 'gfl.user.token' ) );
-    apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name;
+    apiCall.url = repo.API;
     apiCall.cb = function(){
       repo.content = JSON.parse( this.responseText );
-      document.getElementById( "logged" ).style.display = "block";
-
       // ORGANIZATION
       if( repo.content.owner.type == "Organization" ){
+        repo.type = 'org';
         monitor( "game started", repo.content.created_at );
         monitor( "players", repo.content.forks );
         if(repo.content.permissions.admin === true){
-          // GM connected
-          gfl.gmConnected();
+          user.type = 'owner';
         }else{
-          // Guest player
-          gfl.guestConnected();
+          user.type = 'guest';
         }
       }
-
       // PLAYER
       if( repo.content.owner.type == "User" ){
-        if( repo.content.permissions.admin === true ){
-          // Player connected
+        repo.type = 'usr';
+        if(repo.content.fork){
           monitor( "game started", repo.content.parent.created_at );
           monitor( "players", repo.content.parent.forks );
+          if( repo.content.permissions.admin === true ){
+            user.type = 'owner';
+          }else{
+            user.type = 'guest';
+          }
           monitor( "joined game", repo.content.created_at );
-          gfl.checkParentSHA();
         }else{
-          // Player in wrong page
-          gfl.guestConnected();
-          // monitor("You don&apos;t own this repository", "<a href='" + repo.content.parent.html_url + "'>fork</a> your own copy");
+          monitor( "error","user repo is not a fork");
         }
       }
+      gfl.loadScript();
     };
     apiCall.err = function(){
+      repo.type = 'usr';
+      user.type = 'guest';
       // Bad credentials, revoked token
-      gfl.login();
-    };
-    apiCall.call();
-    return "called repo";
-  },
-  login: function(){
-    // LOGIN
-    document.getElementById( "login" ).style.display = "block";
-    document.getElementById( "submitLogin" ).addEventListener('click', function(e) {
-      e.preventDefault();
-      user.token = document.getElementById( "token" ).value;
-      document.getElementById( "token" ).value = '';
-      if (user.token) {
-        document.getElementById( "submitLogin" ).setAttribute("disabled", "true");
-        apiCall.url = "https://api.github.com";
-        apiCall.cb = function(){
-          localStorage.setItem("gfl.user.token", btoa(user.token));
-          appendi('p', 'form', "Authorized. <a href='" + repo.home + "'>Proceed</a>");
-        };
-        apiCall.err = function(){
-          appendi('p', 'form', "Wrong token, retry.");
-          document.getElementById('submitLogin').removeAttribute("disabled");
-          document.getElementById("token").value = '';
-          document.getElementById("token").focus();
-        };
-        apiCall.call();
-      }
-    });
-    return "waiting login";
-  },
-  gmConnected: function(){
-    apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/git/refs/heads/master";
-    apiCall.cb = function(){
-      repo.ref = JSON.parse( this.responseText );
-      monitor( 'version', repo.ref.object.sha.substring(0,7) );
-      gfl.checkFile('leagues.owner.js');
+      user.token = '';
+      localStorage.removeItem("gfl.user.token");
+      gfl.loadScript();
     };
     apiCall.call();
   },
-  checkFile: function(file){
-    if(url.array[0]=='127'){
-      var script = document.createElement("script");
-      script.src = file;
-      script.type = 'text/javascript';
-      document.body.appendChild(script);
-    }else{
-      // load leagues.owner.js
-      apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/contents/core/scripts/" + file;
-      apiCall.data = '{"ref":' + repo.ref.object.sha + '}';
-      apiCall.cb = function(){
-        var script = JSON.parse( this.responseText );
-        var added = document.createElement("script");
-        added.type = 'text/javascript';
-        added.innerHTML = atob(script.content);
-        document.body.appendChild(added);
-      };
-      apiCall.call();
-    }
-  },
-  guestConnected: function(){
-    console.log("guest connected");
-  },
-  checkParentSHA: function(){
-    apiCall.url = "https://api.github.com/repos/" + repo.content.parent.full_name + "/git/refs/heads/master";
-    apiCall.cb = function(){
-      parent.ref = JSON.parse( this.responseText );
-      monitor( 'forked from', '<a href="http://' + repo.content.parent.owner.login + '.github.io/' + repo.content.parent.name + '">' + repo.content.parent.owner.login + '/' + repo.content.parent.name + '</a>' );
-      monitor( 'parent <em>master</em> version', parent.ref.object.sha.substring(0,7) );
-      gfl.checkSHA();
-    };
-    apiCall.err = function(){
-      console.log(this.responseText);
-    };
-    apiCall.call();
-    // url = "https://api.github.com/repos/" + systemRepositoryName + "/git/refs/heads/master";
-    // getAPI( url, systemRef, error, {} );
-  },
-  checkSHA: function(){
-    apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/git/refs/heads/master";
-    apiCall.cb = function(){
-      repo.ref = JSON.parse( this.responseText );
-      if( repo.ref.object.sha == parent.ref.object.sha ){
-        monitor( 'version', repo.ref.object.sha.substring(0,7) );
-        // checkSettings();
-      }else{
-        monitor( '<em>master</em> version', 'need update from ' + repo.ref.object.sha.substring(0,7) );
-        gfl.update();
-        // update();
-      }
-    };
-    apiCall.call();
-  },
-  update: function(){
-    apiCall.url = "https://api.github.com/repos/" + repo.owner + "/" + repo.name + "/git/refs/heads/master";
-    apiCall.accept = 'application/vnd.github.v3.patch';
-    apiCall.method = 'PATCH';
-    apiCall.data = '{"sha":"' + parent.ref.object.sha + '"}';
-    apiCall.cb = function(){
-      gfl.checkSHA();
-    };
-    apiCall.call();
+  loadScript: function(){
+    var script = document.createElement("script");
+    var file = url.script + '.js';
+    if(repo.sha) script.src = repo.static + repo.sha + '/core/scripts/' + file; else script.src = repo.static + 'master/core/scripts/' + file;
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
   }
 };
 
-//
 // API CALL
-//
 apiCall.call = function(){
   var xhr = new XMLHttpRequest();
   xhr.open ( apiCall.method, apiCall.url, true );
   xhr.setRequestHeader( 'Accept', apiCall.accept );
-  xhr.setRequestHeader( 'Authorization', 'token ' + user.token );
+  if(user.token)xhr.setRequestHeader( 'Authorization', 'token ' + user.token );
   xhr.onreadystatechange = function() {
     if ( xhr.readyState == 4 && xhr.status == 200 ) {
       if (typeof apiCall.cb == "function") {
+        if (xhr.getResponseHeader("X-RateLimit-Remaining") < 5) monitor('Rate Limit', 'exceeded');
+        if (xhr.getResponseHeader("X-RateLimit-Remaining") < 2) window.location = repo.home + '/login/';
         var xrate = document.querySelector("footer > small");
         xrate.innerHTML = "X-RateLimit-Remaining: " + xhr.getResponseHeader( "X-RateLimit-Remaining" );
-        // document.querySelector( 'footer' ).appendChild(xrate);
         apiCall.cb.apply( xhr );
       }
     }
@@ -184,13 +106,6 @@ function monitor( name, data ){
   document.getElementById( "monitor" ).appendChild( listItem );
 }
 
-function appendi( what, after, text){
-  var ele = document.createElement(what);
-  ele.innerHTML = text;
-  document.querySelector(after).appendChild(ele);
-}
 
-//
 // START
-//
 var start = gfl.start();
