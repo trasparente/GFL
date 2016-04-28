924b34f1bf3b23d9e8ce648650316285c121634e
Alex Petrosh <petrosh@users.noreply.github.com>
2016-04-28 18:36:59 +0200

    Switch to Object.Create 🎾

--- loader.js
+++ loader.js
@@ -1,32 +1,38 @@
 // loader.js
 
-var url = {},
-  repo = { data: {} };
+function FnP(){
+  this.url = { /* this page url */ };
+  this.url.array = window.location.host.split( '.' );
+  this.url.slash = window.location.pathname.split( '/' );
+  this.url.hash = window.location.hash.substring( 1 );
+  this.url.page = (this.url.slash[2]) ? this.url.slash[2]: false;
+  this.url.setup = (this.url.slash[3]) ? this.url.slash[3]: false;
+  this.repo = { /* this page repository */ };
+  this.repo.owner = this.url.array[0];
+  this.repo.name = this.url.slash[1];
+  this.repo.home = "http://" + this.repo.owner + ".github.io/" + this.repo.name;
+  this.repo.API = "https://api.github.com/repos/" + this.repo.owner + "/" + this.repo.name;
+  this.repo.static = "https://rawgit.com/" + this.repo.owner + "/" + this.repo.name + "/";
+  this.repo.cdn = "https://cdn.rawgit.com/" + this.repo.owner + "/" + this.repo.name + "/";
+  this.repo.data = { /* branch */ };
+  this.repo.team = { /* repo team */ };
+}
 
-url.array = window.location.host.split( '.' );
-url.slash = window.location.pathname.split( '/' );
-url.hash = window.location.hash.substring( 1 );
-repo.owner = url.array[0];
-repo.name = url.slash[1];
-if(url.slash[2]) url.page = url.slash[2];
-if(url.slash[3]) url.setup = url.slash[3];
-repo.home = "http://" + repo.owner + ".github.io/" + repo.name;
-repo.API = "https://api.github.com/repos/" + repo.owner + "/" + repo.name;
-repo.static = "https://rawgit.com/" + repo.owner + "/" + repo.name + "/";
-repo.cdn = "https://cdn.rawgit.com/" + repo.owner + "/" + repo.name + "/";
+var fnp = new FnP();
+
 function loadAssets() {
   var script = document.createElement("script");
-  script.src = repo.static + "master/scripts/init.js";
+  script.src = fnp.repo.static + "master/scripts/init.js";
   script.type = 'text/javascript';
   document.body.appendChild(script);
-  if((url.page && url.page == 'setup') || (url.setup && url.page)){
+  if((fnp.url.page && fnp.url.page == 'setup') || (fnp.url.page && fnp.url.setup)){
     var jsoneditor = document.createElement("script");
-    jsoneditor.src = repo.static + "master/scripts/jsoneditor.js";
+    jsoneditor.src = fnp.repo.static + "master/scripts/jsoneditor.js";
     jsoneditor.type = 'text/javascript';
     document.body.appendChild(jsoneditor);
   }
   var style = document.createElement("link");
-  style.href = repo.static + "master/styles/style.css";
+  style.href = fnp.repo.static + "master/styles/style.css";
   style.rel = 'stylesheet';
   document.head.appendChild(style);
 }
