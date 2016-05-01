3956e14bf83bdeec7b66ad1f1a8d5b5d62c87398
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-01 23:51:10 +0200

    It's Too Late ⏰

--- scripts/loader.js
+++ scripts/loader.js
@@ -189,7 +189,7 @@ fnp.getMasterHead = function(){
   fnp.apiCall({
     url: fnp.repo.API + "/git/refs/heads/master",
     cb: function(){
-      fnp.repo.master = fnp.repo.master != 'master' ? this.object.sha : 'master';
+      fnp.repo.master = fnp.repo.master != 'master' ? fnp.repo.master : this.object.sha;
       fnp.getThisRepo();
     }
   });
