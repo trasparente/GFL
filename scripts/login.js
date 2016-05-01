078d820ee5661ccd05da716d61d710146d7e5b24
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-01 15:42:32 +0200

    Add apiCall ☎️

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,14 @@
 document.getElementById( "submitLogin" ).addEventListener('click', function(e) {
   e.preventDefault();
-  user.token = document.getElementById( "token" ).value;
+  fnp.user.token = document.getElementById( "token" ).value;
   document.getElementById( "token" ).value = '';
-  if (user.token) {
+  if (fnp.user.token) {
     document.getElementById( "submitLogin" ).setAttribute("disabled", "true");
+    fnp.apiCall( "https://api.github.com", fnp.repo.API, function(){
+      localStorage.setItem("fnp.user.token", btoa(user.token));
+      window.location = repo.home;
+    });
+
     apiCall.url = "https://api.github.com";
     apiCall.cb = function(){
       localStorage.setItem("fnp.user.token", btoa(user.token));
@@ -16,5 +21,13 @@ document.getElementById( "submitLogin" ).addEventListener('click', function(e) {
       document.getElementById("token").focus();
     };
     apiCall.call();
+  }
+});
+
+this.apiCall( fnp.repo.API, function(){
+  if(resp){
+    console.log('ok');
+  }else{
+    console.log('no');
   }
 });
