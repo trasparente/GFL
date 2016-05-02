29726b3585fbf829d30c23763f17f3d582cf8c41
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-02 20:06:33 +0200

    Try to Show the INVISIBLE ♨️

--- scripts/home.js
+++ scripts/home.js
@@ -12,17 +12,17 @@ fnp.home = {
         fnp.setup.default = JSON.parse( atob(this.content) );
         fnp.setup.sha = this.sha;
         if(fnp.repo.type == 'Organization' && fnp.user.type == 'owner') {
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'setup: <a href="' + fnp.repo.home + '/setup">edit</a>' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'setup: <a href="' + fnp.repo.home + '/setup">edit</a>' } });
         }else{
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'setup: found' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'setup: found' } });
         }
         fnp.home.checkLeagues();
       },
       err: function(){
         if(fnp.repo.type == 'Organization' && fnp.user.type == 'owner'){
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'warning: no setup, <a href="' + fnp.repo.home + '/setup/">create</a>' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'warning: no setup, <a href="' + fnp.repo.home + '/setup/">create</a>' } });
         }else{
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'error: no setup' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'error: no setup' } });
         }
       }
     });
@@ -34,16 +34,16 @@ fnp.home = {
         fnp.leagues.content = this;
         fnp.leagues.obj = JSON.parse( atob(this.content) );
         if(fnp.repo.type == 'Organization' && fnp.user.type == 'owner'){
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'leagues: <a href="' + fnp.repo.home + '/league/setup">edit</a>' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'leagues: <a href="' + fnp.repo.home + '/league/setup">edit</a>' } });
         }else{
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'leagues: found' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'leagues: found' } });
         }
       },
       err: function(){
         if(fnp.repo.type == 'Organization' && fnp.user.type == 'owner'){
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'warning: no leagues, <a href="' + fnp.repo.home + '/league/setup/">create</a>' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'warning: no leagues, <a href="' + fnp.repo.home + '/league/setup/">create</a>' } });
         }else{
-          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { innerHTML: 'error: no leagues' } });
+          fnp.appendi({ tag: 'li', parent: fnp.dom.ul, attributes: { 'innerHTML': 'error: no leagues' } });
         }
       }
     });
