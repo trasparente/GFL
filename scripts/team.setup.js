7083758ef353da22e76f82b712de9901bf1b63a4
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-19 13:20:14 +0200

    Decode schemas with `atob`

--- scripts/team.setup.js
+++ scripts/team.setup.js
@@ -42,7 +42,7 @@ fnp.team = {
     fnp.apiCall({
       url: fnp.searchMasterFile('schema/setup.json'),
       cb: function(){
-        fnp.setup.schema = JSON.parse( fnp.b64d(this.content) );
+        fnp.setup.schema = JSON.parse( atob(this.content) );
         // Initialize the editor
         var editor = new JSONEditor(fnp.dom.editor,{
           ajax: true,
