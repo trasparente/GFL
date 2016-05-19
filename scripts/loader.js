0ce9d8c784de2549c16561712839392f6ce55519
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-19 13:51:32 +0200

    Exclude ETag check in apiFirstCall and fix fnp.user.toke getter

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,25 +1,19 @@
 // loader.js
 
 fnp.user = {
-  get token() { if(localStorage.getItem( 'fnp.user.token')) return atob( localStorage.getItem( 'fnp.user.token' ) ); else return false; }
+  get token() { return (localStorage.getItem( 'fnp.user.token')) ? atob( localStorage.getItem( 'fnp.user.token' ) ) : false; }
 };
 
 fnp.apiFirstCall = function(obj){
   obj.method = 'GET';
   obj.accept = 'application/vnd.github.v3.full+json';
-  obj.etag = 'fnp.etag.' + obj.url.replace(/\W+/g, "");
   var xhr = new XMLHttpRequest();
   xhr.open ( obj.method, obj.url, true );
   xhr.setRequestHeader( 'Accept', obj.accept );
   if(fnp.user.token) xhr.setRequestHeader( 'Authorization', 'token ' + fnp.user.token );
-  if(localStorage.getItem(obj.etag)) xhr.setRequestHeader('If-None-Match', localStorage.getItem(obj.etag));
   xhr.onreadystatechange = function() {
     if(xhr.readyState == 4){
       document.querySelector('body').style.backgroundColor = "white";
-      if(xhr.getResponseHeader('ETag') && obj.url.indexOf('ref=') == -1 ){
-        var etag = RegExp(/W\/"(.*?)"/).exec(xhr.getResponseHeader('ETag'));
-        if(etag) localStorage.setItem(obj.etag, etag[1]);
-      }
       if ( xhr.status == 200 ||  xhr.status == 201 ||  xhr.status == 204 ) {
         if (typeof obj.cb == 'function') {
           if (xhr.getResponseHeader('X-RateLimit-Remaining') < 5) fnp.appendi({ tag: 'li', parent: fnp.dom.ul, innerHTML: 'rate limit: exceeded' });
