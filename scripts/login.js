f8fcf05ce7e0e929bd5bde0854f628a35862bcec
Alex Petrosh <petrosh@users.noreply.github.com>
2016-05-20 16:57:36 +0200

    Get rid of getter and setter

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,13 @@
 document.getElementById( 'submitLogin' ).addEventListener('click', function(e) {
   e.preventDefault();
-  fnp.user.token = document.getElementById( 'token' ).value;
+  var token = document.getElementById( 'token' ).value;
   document.getElementById( 'token' ).value = '';
-  if (fnp.user.token) {
+  if (token) {
     document.getElementById( 'submitLogin' ).setAttribute('disabled', 'true');
     fnp.apiFirstCall({
       url: 'https://api.github.com',
       cb: function(){
-        localStorage.setItem('fnp.user.token', btoa(fnp.user.token));
+        localStorage.setItem('fnp.user.token', btoa(token));
         fnp.appendi({ tag: 'li', parent: fnp.dom.ul, innerHTML: 'logged: <a href="' + fnp.repo.home + '">proceed</a>' });
       },
       err: function(){
