07aa2abb25d7e43626e5c0557dd0d54674a2f689
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-06 16:36:39 +0200

    Introduce domSection appends

--- scripts/login.js
+++ scripts/login.js
@@ -11,7 +11,7 @@ eleSubmit.addEventListener('click', function(e) {
       url: 'https://api.github.com',
       cb: function(){
         localStorage.setItem('userToken', btoa(token));
-        domAppend({ tag: 'li', parent: monitorString, innerHTML: 'logged: <a href="' + repoHome + '">proceed</a>' });
+        domAppend({ tag: 'li', parent: domSection, innerHTML: 'logged: <a href="' + repoHome + '">proceed</a>' });
       },
       err: function(){
         domAppend({ tag: 'li', parent: monitorString, innerHTML: 'error: wrong token, try again' });
