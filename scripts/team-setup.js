f029ac0a236921395a1d75e158661bca0b3d7e08
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-09 23:37:18 +0200

    Team already readed

--- scripts/team-setup.js
+++ scripts/team-setup.js
@@ -7,47 +7,36 @@ if (userType != 'owner' || repoType != 'User') window.location = repoHome;
 setupEditor();
 
 apiCall({
-  url: fileUrl('teams', 'teams/' + repoOwner + '.json'),
-  cb: function(){
-    jsonTeam = JSON.parse( b64d(this.content) );
-    domAppend({ tag: 'li', parent: domUlGame, innerHTML: 'team: found' });
-    loadSetup();
+  url: fileUrl('master', 'schema/setup.json'),
+  cb: function() {
+    setupSchema = JSON.parse(atob(this.content));
+    // Initialize the editor
+    var editor = new JSONEditor(domEditor,{
+      ajax: true,
+      schema: setupSchema.properties.team,
+      startval: jsonTeam,
+      no_additional_properties: false,
+      required_by_default: false,
+      // Special
+      disable_properties: true,
+      disable_edit_json: true,
+      disable_array_reorder: false
+    });
+    domSubmit.addEventListener('click',function () { saveTeam(editor.getValue()); });
+    domCancel.addEventListener('click',function () { window.location = repoHome; });
+    editor.on('change',function() {
+      var errors = editor.validate();
+      if (errors.length) {
+        console.log(errors);
+        domValid.style.color = 'red';
+        domValid.textContent = "not valid";
+      } else {
+        domValid.style.color = 'green';
+        domValid.textContent = "valid";
+      }
+    });
   }
 });
-
-function loadSetup() {
-  apiCall({
-    url: fileUrl('master', 'schema/setup.json'),
-    cb: function() {
-      setupSchema = JSON.parse( atob(this.content) );
-      // Initialize the editor
-      var editor = new JSONEditor(domEditor,{
-        ajax: true,
-        schema: setupSchema.properties.team,
-        startval: jsonTeam,
-        no_additional_properties: false,
-        required_by_default: false,
-        // Special
-        disable_properties: true,
-        disable_edit_json: true,
-        disable_array_reorder: false
-      });
-      domSubmit.addEventListener('click',function () { saveTeam(editor.getValue()); });
-      domCancel.addEventListener('click',function () { window.location = repoHome; });
-      editor.on('change',function() {
-        var errors = editor.validate();
-        if (errors.length) {
-          console.log(errors);
-          domValid.style.color = 'red';
-          domValid.textContent = "not valid";
-        } else {
-          domValid.style.color = 'green';
-          domValid.textContent = "valid";
-        }
-      });
-    }
-  });
-}
 
 function saveTeam (dati) {
   hideEditor();
