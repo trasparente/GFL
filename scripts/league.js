a67716f93104a01ba10b4889a804fcddeac44ca0
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-09 14:17:31 +0200

    Pass leagueObj to `showLeagues`

--- scripts/league.js
+++ scripts/league.js
@@ -4,8 +4,9 @@ var leagueSlug = false;
 
 if(urlHash && urlHash.slice(0,7) === 'league=') leagueSlug = urlHash.slice(7);
 
-var leagueObj = jsonLeagues.filter(function( obj ) {
+var leagueObj = jsonLeagues.filter(function (obj) {
   return obj.slug == leagueSlug;
 });
 
+if(leagueObj.length) showLeagues(leagueObj);
 console.log(leagueObj);
