4392980213b45af082e4875f56540bfe1c862738
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-09 14:16:59 +0200

    Move showLeagues in `upldater`

--- scripts/home.js
+++ scripts/home.js
@@ -1,23 +1,3 @@
 // home.js
 
-function showLeagues(){
-  for( i=0; i < jsonLeagues.length; i++ ){
-    var row = document.createElement('tr');
-    row.innerHTML = '<td><a href="' + repoHome + '/league/#league=' + jsonLeagues[i].slug + '">' + jsonLeagues[i].title + '</a></td>';
-    domTable.appendChild(row);
-  }
-  domSection.appendChild(domTable);
-  showReadme();
-}
-
-function showReadme(){
-  apiCall({
-    url: repoAPI + '/readme?ref=' + 'gh-pages',
-    accept: 'application/vnd.github.v3.html',
-    cb: function(){
-      domAppend({ tag: 'div', parent: domSection, innerHTML: this });
-    }
-  });
-}
-
-if(jsonLeagues.length) showLeagues();
+if(jsonLeagues.length) showLeagues(jsonLeagues);
